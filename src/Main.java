8100eb555925d78a7861bc25d62c8995316d0b9b
laroself1 <laroself1@gmail.com>
2016-02-22 17:07:32 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       Person M = new Person();
+        M.set1("Pavel","Pyatkin",34,"M",0631214552);
+        M.set1('P','P',34.5,1,"0631214552");
+        M.set1('P','p',338789337,true,0631214552); }}
---
+++ src/Person.java
@@ -0,0 +1,14 @@
+public class Person {
+
+    public String firstName;
+    public  String lastName;
+    public int age;
+    public String gender;
+    public int phoneNum;
+
+    public void set1(String firstName,  String lastName,int age,String gender,int phoneNum)
+    {System.out.println(firstName+" "+lastName+" "+age+" "+gender+" "+phoneNum);}
+    public void set1(char firstName,  char lastName, double age, int gender,String phoneNum)
+    {System.out.println(firstName+" "+lastName+" "+age+" "+gender+" "+phoneNum);}
+    public void set1(char firstName,  char lastName, long age, boolean gender,long phoneNum)
+    {System.out.println(firstName+" "+lastName+" "+age+" days "+gender+" "+phoneNum);}}
